b665a83acf08a8fd84c750efeb1e06b8cc224569
cxzucc <cxzucc@163.com>
2015-01-17 20:17:58 +0800

    初始化提交。

---
+++ service/src/main/java/com/kaisen/wirelessportal/service/ISayHelloWorld.java
@@ -0,0 +1,7 @@
+package com.kaisen.wirelessportal.service;
+
+public interface ISayHelloWorld {
+	public String sayHelloWorld(String userName);
+
+	public String sayHelloWorld_JSON(String userName);
+}
